a25d76877591caf416dd3d5505200ab331878c87
root <root@localhost.localdomain>
2024-03-28 20:42:32 +0000

    Terminado el ejercicio 3

--- p1/entrega.ml
+++ p1/entrega.ml
@@ -35,7 +35,7 @@ let rec indeterminismosL = function
 	| _ :: tl -> indeterminismosL tl;;
 
 
-
+(* Funciones ejercicio *)
 
 let es_afne (Af (_, _, _, arcos, _)) =
   e_transicion (list_of_conjunto arcos);;
@@ -53,10 +53,63 @@ let es_afd af =
 (* Ejercicio 2 *)
 
 
+(*-----------------------------------------------------------------------------------------------*)
+
+(* Ejercicio 3 *)
 
 
+(* Elementos para las pruebas *)
+(* let afd = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c;";; *)
+(* let afn = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
+(* let afnn = af_of_string "0 1 2 3; a b c; 0; 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
+(* let nn = [Terminal "a"; Terminal "b"];; *)
+(* let an = [Terminal "a"; Terminal "a"; Terminal "c"];; *)
+(* let nd = [Terminal "a"; Terminal "b"; Terminal "a"];; *)
+(* let ad = [Terminal "a"; Terminal "b"; Terminal "b"; Terminal "a"; Terminal "c"];; *)
 
+(* funciones auxiliares *)
 
+let avanza simbolo estados (Af (_, _, _, Conjunto arcos, _)) =
 
+   let rec aux destinos = function
+
+        [] -> destinos
+
+      | Arco_af (origen, destino, s) :: t ->
+           if (s = simbolo) && (pertenece origen estados) then
+              aux (agregar destino destinos) t
+           else
+              aux destinos t
+
+   in
+      aux conjunto_vacio arcos
+   ;;
+   
+let rec avanzaDeterminista (Terminal simbolo) (Estado estado) = function
+	| [] -> ((Estado estado), None)
+	| Arco_af (Estado est, Estado estRes, Terminal v) :: tl -> if est = estado && v = simbolo then ((Estado estRes), Some v) else avanzaDeterminista (Terminal simbolo) (Estado estado) tl
+	| _ :: tl -> avanzaDeterminista (Terminal simbolo) (Estado estado) tl;;
+		
+
+(* Funciones del ejercicio *)
+let escaner_afn cadena (Af (_, _, inicial, _, finales) as a) = 
+	let rec aux = function
+	| (Conjunto [], _) -> false
+	| (actuales, []) -> not (es_vacio (interseccion actuales finales))
+	| (actuales, s::ss) -> aux (avanza s actuales a, ss)
+	in aux ((Conjunto [inicial]), cadena);;
+	
+	
+let escaner_afd cadena (Af (_, _, inicial, arcos, finales)) =
+	let rec aux fin arcos= function
+		| [] -> (pertenece fin finales)
+		| s::ss -> let estado, tvalue = (avanzaDeterminista s fin arcos) in 
+			match tvalue with
+				| Some v -> aux estado arcos ss
+				| None -> false 
+		in aux inicial (list_of_conjunto arcos) cadena;;
 
 	
