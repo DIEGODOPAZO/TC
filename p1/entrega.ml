281d026ada71f5ef8bffb888ef5e666b654973e0
root <root@localhost.localdomain>
2024-03-29 16:18:00 +0000

    Arreglada el ejer1

--- p1/entrega.ml
+++ p1/entrega.ml
@@ -12,9 +12,13 @@ open Graf;;
 (* let afne = af_of_string "0 1 2 3; a b c; 0; 1 3;
 0 1 a; 1 1 b; 1 2 a; 2 0 epsilon; 2 3 epsilon; 2 3 c;";; *)
 (* let afd = af_of_string "0 1 2 3; a b c; 0; 1 3;
-0 1 a; 1 1 b; 1 2 a; 2 3 c;";; *)
+0 1 a; 0 2 b; 0 3 c; 1 3 c; 1 1 b; 1 2 a; 2 3 c; 2 1 a; 2 2 b; 3 3 a; 3 3 b; 3 3 c;";; *)
+(* let afnnn = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 0 2 b; 0 3 c; 1 3 c; 1 1 b; 1 2 a; 2 3 c; 2 1 a; 2 2 b;";; *)
+(* let afnn = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 0 2 b; 0 3 c; 1 3 c; 1 1 b; 1 2 a; 2 3 c; 2 1 a;";; *)
 (* let afn = af_of_string "0 1 2 3; a b c; 0; 1 3;
-0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a";; *)
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
 
 (* funciones auxiliares *)
 let rec e_transicion= function 
@@ -23,18 +27,22 @@ let rec e_transicion= function
   | _ :: resto ->  e_transicion resto;;
 
 
-let rec indeterminismosE estado valor res= function
-	| [] -> false
-	| Arco_af (_, _, Terminal "") :: tl -> indeterminismosE estado valor res tl
-	| Arco_af (Estado est, Estado estRes, Terminal v) :: tl -> if est = estado && v = valor && estRes != res then true else indeterminismosE estado valor res tl
-	| _ :: tl -> indeterminismosE estado valor res tl;;
-
-let rec indeterminismosL = function
-	| [] -> false
-	| Arco_af (Estado estado, Estado res, Terminal valor) :: tl -> if indeterminismosE estado valor res tl then true else indeterminismosL tl
-	| _ :: tl -> indeterminismosL tl;;
+let rec indeterminismosE estado valor res valoresE valoresT estV= function
+	| [] ->  if (igual valoresE valoresT) then false else (if (pertenece estado estV) then false else true)
+	| Arco_af (_, _, Terminal "") :: tl -> indeterminismosE estado valor res valoresE valoresT estV tl (* or false si no se quiere q tenga e arcos*)
+	| Arco_af (Estado est, Estado estRes, Terminal v) :: tl -> if est = estado && v = valor && estRes != res then true else (if est = estado then indeterminismosE estado valor res (agregar (Terminal v) valoresE) valoresT estV tl else indeterminismosE estado valor res valoresE valoresT estV tl)
+	| _ :: tl -> indeterminismosE estado valor res valoresE valoresT estV tl;;
 
 
+let rec indeterminismosL estadosT valoresT estV= function
+	| [] -> not (igual estadosT estV)
+	| Arco_af (Estado estado, Estado res, Terminal valor) :: tl -> if indeterminismosE estado valor res (conjunto_of_list [Terminal valor]) valoresT estV tl then true else indeterminismosL estadosT valoresT (agregar estado estV) tl
+	| _ :: tl -> indeterminismosL estadosT valoresT estV tl;;
+
+let estado_to_string (Estado s) = s;;
+let rec estados_to_strings res = function
+  | [] -> res
+  | h::t -> estados_to_strings (agregar (estado_to_string h) res) t;;
 (* Funciones ejercicio *)
 
 let es_afne (Af (_, _, _, arcos, _)) =
@@ -42,7 +50,7 @@ let es_afne (Af (_, _, _, arcos, _)) =
   
 let es_afn af =
 	match af with 
-	| Af (_, _, _, arcos, _) -> indeterminismosL (list_of_conjunto arcos);;
+	| Af (estadosT, valoresT, _, arcos, _) -> indeterminismosL (estados_to_strings (conjunto_of_list []) (list_of_conjunto estadosT)) valoresT (conjunto_of_list []) (list_of_conjunto arcos);;
 	
 let es_afd af = 
 	not (es_afn af) && not (es_afne af);;
@@ -61,11 +69,14 @@ let es_afd af =
 (* Elementos para las pruebas *)
 (* let afd = af_of_string "0 1 2 3; a b c; 0; 1 3;
 0 1 a; 1 1 b; 1 2 a; 2 3 c;";; *)
-(* let afn = af_of_string "0 1 2 3; a b c; 0; 1 3;
-0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
+(* let afn = af_of_string "0 1 2 3; a b c ws; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a; 0 3 ws;";; *)
+(* let afn2 = af_of_string "0 1 2 3 4; a b c ws; 0; 4;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a; 0 4 ws;";; *)
 (* let afnn = af_of_string "0 1 2 3; a b c; 0; 3;
 0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
 (* let nn = [Terminal "a"; Terminal "b"];; *)
+(* let an = [Terminal "a"; Terminal "a"; Terminal "c"];; *)
 (* let an = [Terminal "a"; Terminal "a"; Terminal "c"];; *)
 (* let nd = [Terminal "a"; Terminal "b"; Terminal "a"];; *)
 (* let ad = [Terminal "a"; Terminal "b"; Terminal "b"; Terminal "a"; Terminal "c"];; *)
