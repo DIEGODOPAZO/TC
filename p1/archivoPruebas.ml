0ef94146de3d89639f58b45a67ebef0902c5e7ea
root <root@localhost.localdomain>
2024-04-06 20:32:54 +0100

    Preparacion para la entrega de la tarea

--- p1/archivoPruebas.ml
+++ p1/archivoPruebas.ml
@@ -7,6 +7,19 @@ open Graf;;
 
 (* Ejercicio 1 *)
 
+(* Elementos para las pruebas *)
+
+(* let afne = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 0 epsilon; 2 3 epsilon; 2 3 c;";; *)
+(* let afd = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 0 2 b; 0 3 c; 1 3 c; 1 1 b; 1 2 a; 2 3 c; 2 1 a; 2 2 b; 3 3 a; 3 3 b; 3 3 c;";; *)
+(* let afnnn = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 0 2 b; 0 3 c; 1 3 c; 1 1 b; 1 2 a; 2 3 c; 2 1 a; 2 2 b;";; *)
+(* let afnn = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 0 2 b; 0 3 c; 1 3 c; 1 1 b; 1 2 a; 2 3 c; 2 1 a;";; *)
+(* let afn = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
+
 (* funciones auxiliares *)
 let rec e_transicion= function 
   | [] -> false
@@ -46,6 +59,108 @@ let es_afd af =
 (*-----------------------------------------------------------------------------------------------*)
 
 (* Ejercicio 2 *)
+
+(* Elementos para probar*)
+
+(* Los tres primeros son equivalentes entre ellos, los dos ultimos no son equivalentes a ninguno*)
+
+(*
+let eq_afne = Af (
+    Conjunto [Estado "0"; Estado "1"; Estado "2"; Estado "3"; Estado "4"],
+
+    Conjunto [Terminal "a"; Terminal "b"],
+
+    Estado "0",
+
+    Conjunto [
+      Arco_af (Estado "0", Estado "3", Terminal "a");
+      Arco_af (Estado "0", Estado "1", Terminal "");
+      Arco_af (Estado "1", Estado "2", Terminal "");
+      Arco_af (Estado "1", Estado "2", Terminal "a");
+      Arco_af (Estado "3", Estado "4", Terminal "b");
+      Arco_af (Estado "4", Estado "0", Terminal "b");
+      Arco_af (Estado "4", Estado "1", Terminal "")
+    ],
+
+    Conjunto [Estado "2"]
+  );;
+
+let eq_afn = Af (
+    Conjunto [Estado "0"; Estado "1"; Estado "2"; Estado "3"; Estado "4"],
+
+    Conjunto [Terminal "a"; Terminal "b"],
+
+    Estado "0",
+
+    Conjunto [
+      Arco_af (Estado "0", Estado "3", Terminal "a");
+      Arco_af (Estado "0", Estado "2", Terminal "a");
+      Arco_af (Estado "1", Estado "2", Terminal "a");
+      Arco_af (Estado "3", Estado "1", Terminal "b");
+      Arco_af (Estado "3", Estado "2", Terminal "b");
+      Arco_af (Estado "3", Estado "4", Terminal "b");
+      Arco_af (Estado "4", Estado "1", Terminal "b");
+      Arco_af (Estado "4", Estado "2", Terminal "a");
+      Arco_af (Estado "4", Estado "2", Terminal "b");
+      Arco_af (Estado "4", Estado "0", Terminal "b");
+      ],
+
+    Conjunto [Estado "0"; Estado "1"; Estado "2"; Estado "4";]
+  );;
+
+
+let eq_afd = Af (
+    Conjunto [Estado "0"; Estado "23"; Estado "124"; Estado "2"; Estado "012"],
+
+    Conjunto [Terminal "a"; Terminal "b"],
+
+    Estado "0",
+
+    Conjunto [
+      Arco_af (Estado "0", Estado "23", Terminal "a");
+      Arco_af (Estado "23", Estado "124", Terminal "b");
+      Arco_af (Estado "124", Estado "2", Terminal "a");
+      Arco_af (Estado "124", Estado "012", Terminal "b");
+      Arco_af (Estado "012", Estado "23", Terminal "a");
+      ],
+
+    Conjunto [Estado "0"; Estado "23"; Estado "124"; Estado "2"; Estado "012"]
+  );;
+  
+let neq_afd = Af (
+    Conjunto [Estado "0"; Estado "23"; Estado "124"; Estado "2"; Estado "012"; Estado "fin"],
+
+    Conjunto [Terminal "a"; Terminal "b"],
+
+    Estado "0",
+
+    Conjunto [
+      Arco_af (Estado "0", Estado "23", Terminal "a");
+      Arco_af (Estado "23", Estado "fin", Terminal "b");
+      Arco_af (Estado "124", Estado "2", Terminal "a");
+      Arco_af (Estado "124", Estado "012", Terminal "b");
+      Arco_af (Estado "012", Estado "23", Terminal "a");
+      ],
+
+    Conjunto [Estado "0"; Estado "23"; Estado "124"; Estado "2"; Estado "012"]
+  );;
+ let neq = Af (
+    Conjunto [Estado "0"; Estado "23"; Estado "124"; Estado "2"; Estado "012"; Estado "fin"],
+
+    Conjunto [Terminal "a"; Terminal "b"],
+
+    Estado "0",
+
+    Conjunto [
+      Arco_af (Estado "0", Estado "23", Terminal "a");
+      Arco_af (Estado "23", Estado "fin", Terminal "b");
+      Arco_af (Estado "012", Estado "23", Terminal "a");
+      ],
+
+    Conjunto [Estado "0"; Estado "23"; Estado "124"; Estado "2"; Estado "012"]
+  );;
+*)
+
 
 (* Funciones auxiliares *)
 
@@ -89,8 +204,6 @@ let get_E_transiciones (Af (_, alfabeto, inicial, arcos, finales)) estado=
 
 let allVisited est1 est2 vis1 vis2 = 
 	if (incluido est1 vis1) && (incluido est2 vis2) then true else false ;;	
-	
-	
 (* Funcion del ejercicio*)
 
 let equivalentes (Af (estados1, alfabeto1, inicial1, arcos1, finales1) as a) (Af (estados2, alfabeto2, inicial2, arcos2, finales2) as b) = 
@@ -109,6 +222,22 @@ let equivalentes (Af (estados1, alfabeto1, inicial1, arcos1, finales1) as a) (Af
 (*-----------------------------------------------------------------------------------------------*)
 
 (* Ejercicio 3 *)
+
+
+(* Elementos para las pruebas *)
+(* let afd = af_of_string "0 1 2 3; a b c; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c;";; *)
+(* let afn = af_of_string "0 1 2 3; a b c ws; 0; 1 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a; 0 3 ws;";; *)
+(* let afn2 = af_of_string "0 1 2 3 4; a b c ws; 0; 4;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a; 0 4 ws;";; *)
+(* let afnn = af_of_string "0 1 2 3; a b c; 0; 3;
+0 1 a; 1 1 b; 1 2 a; 2 3 c; 0 0 a;";; *)
+(* let nn = [Terminal "a"; Terminal "b"];; *)
+(* let an = [Terminal "a"; Terminal "a"; Terminal "c"];; *)
+(* let an = [Terminal "a"; Terminal "a"; Terminal "c"];; *)
+(* let nd = [Terminal "a"; Terminal "b"; Terminal "a"];; *)
+(* let ad = [Terminal "a"; Terminal "b"; Terminal "b"; Terminal "a"; Terminal "c"];; *)
 
 (* funciones auxiliares *)
 
